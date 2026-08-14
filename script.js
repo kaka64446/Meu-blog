1b7cc4ca7ab011b4bc3da8f1bbe44613bf98e639
kaka64446 <kathleennximenes@gmail.com>
2026-08-13 23:21:44 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const botaoComprar = document.querySelector('.btn-comprar');
+if (botaoComprar) {
+  botaoComprar.addEventListener('click', () => {
+    console.log
+  });
+}
+const cardProduto = document.querySelector('.card-produto');
+if (cardProduto) {
+  cardProduto.addEventListener('mouseenter', () => {
+    cardProduto.style.boxShadow = '0 0 20px rgba(212, 175, 55, 0.6)';
+    cardProduto.style.transition = 'box-shadow 0.3s ease';
+  });
+ cardProduto.addEventListener('mouseleave', () => {
+    cardProduto.style.boxShadow = '0 4px 12px rgba(212, 175, 55, 0.15)';
+  });
+}
